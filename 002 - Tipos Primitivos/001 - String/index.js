7cf863d4a1df0f7453a3f8d9f82193356e16a367
Gabriel Reis <gabriel.reis@revizia.com.br>
2025-06-02 15:34:29 -0300

    first commit

---
+++ 002 - Tipos Primitivos/001 - String/index.js
@@ -0,0 +1,12 @@
+console.log("Aqui vai uma string");
+console.log('"Aqui vai uma string"');
+console.log(`Aqui vai uma string`);
+
+
+console.log(String())
+/* 
+Como podemos escrever uma string
+'Aqui vai uma string'
+"Aqui vai uma string"
+´Aqui vai uma string´
+*/
