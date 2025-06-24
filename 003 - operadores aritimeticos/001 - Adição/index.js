4852de31f496ad85f765613515775dd66cc794ac
Gabriel Correia Reis <correiareisg@gmail.com>
2025-06-23 21:41:35 -0300

    main

---
+++ 003 - operadores aritimeticos/001 - Adição/index.js
@@ -0,0 +1,6 @@
+console.log(1 + 1);
+console.log(25 + 50);
+
+console.log("Denner " + "Goçalves" );
+console.log("Nome: " + "Gabriel Corre" + "," + "idade: " + 19 + 1);
+console.log("Nome: " + "Gabriel Correia" + "," + "idade: " + (19 + 1));  
