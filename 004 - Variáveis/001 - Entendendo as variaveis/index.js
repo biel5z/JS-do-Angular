b89944ac04df467b695d430fbb91c40a81c2cb00
Gabriel Reis <gabriel.reis@revizia.com.br>
2025-07-31 11:15:52 -0300

    main

--- 004 - Variáveis/001 - Entendendo as variaveis/index.js
+++ 004 - Variáveis/001 - Entendendo as variaveis/index.js
@@ -3,3 +3,16 @@ var // Ele é utilizado globalmente
 let // Ele é geradado dentro de um escopo
 const // Ele é gerado dentro de yum escopo porém podemos armazenar dados apenas 1x
 */
+
+var podemos = "palmeiras";
+console.log(podemos);
+
+podemos = "cozinha"
+console.log(podemos);
+
+
+// let lanche = "Bigmac";
+// console.log(lanche);
+
+// const aborora = "doce";
+// console.log(aborora);
