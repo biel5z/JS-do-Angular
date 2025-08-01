da0449ae596453fd3f2efd49f8f1d82033fe1c3c
Gabriel Reis <gabriel.reis@revizia.com.br>
2025-07-31 22:39:59 -0300

    Condicionais - if else

---
+++ 005 - Condicionais/001 - if - else/index.js
@@ -0,0 +1,15 @@
+// let soma = 5;
+
+// if (soma >= 5){
+//     console.log("Você está com o saldo positivo");
+// } else {
+//     console.log("Você está com o saldo negativo");
+// }
+
+if (1 === 0){
+
+} else if(1 === 1){
+
+}else{
+    
+}
