a744352fb2ef9bb7252a55aae27737103cb30cf0
Gabriel Reis <gabriel.reis@revizia.com.br>
2025-08-02 17:54:19 -0300

    Cond - operador ternario

---
+++ 005 - Condicionais/003 - operador ternario/index.js
@@ -0,0 +1,10 @@
+const velocidade = 86;
+const warn = 55;
+
+const codicao = velocidade >= warn ? console.log("Recebeu uma Multa!") : console.log("Continua Andando!")
+
+// if (velocidade >= warn ){
+//     console.log("Recebeu uma Multa!")
+// } else {
+//     console.log("Continua andando!")
+// }
