2f95f88a5cdae53e83ef5b8ad97d65ddf8d23ff1
Gabriel Correia Reis <correiareisg@gmail.com>
2025-08-04 01:13:41 -0300

    Exer - calculadora IMC

---
+++ 006 - exercicios/001 - calculadora IMC/index.js
@@ -0,0 +1,59 @@
+/*
+IMC é a sigla para indicie de Massa Corpórea,
+parãmetro adotado pela Organização Mundial
+de Saúde para calcular o peso idela de cada pessoa.
+
+Como calcular o IMC?
+O IMC é calculado dividindo o peso (em kg)pela
+altura ao quadrado (em m), de acordo com a seguinte
+fórmula: IMC = peso / (altura x altura).
+
+E nosso calculo devemos retornar os seguintes valores para o usuário:
+
+Resultado do Calc     | Resuldado para o usuário
+Abaixo de 17          | Muito abaixo do peso
+Entre 17 e 18.49      | Abaixo do peso
+Entre 18,5 e 24.99    | Peso normal
+Entre 25 e 29.99      | Acima do Peso
+Entre 30 e 34.99      | Obesidade I
+Entre 35 e 39.99      | Obesidade II
+
+*/
+
+const peso = 157;
+const altura = 1.87;
+const imc = Number((peso / (altura * altura)).toFixed(2));
+
+
+switch (true) {
+    case imc < 17:
+        console.log("Muito abaixo do peso")
+        break;
+
+    case imc >= 17 && imc <= 18.49:
+        console.log("Abaixo do peso")
+        break;
+
+    case imc >= 18.5 && imc <= 24.99:
+        console.log("Peso normal")
+        break;
+
+    case imc >= 25 && imc <= 29.99:
+        console.log("Acima do Peso")
+        break;
+
+    case imc >= 30 && imc <= 34.99:
+        console.log("Obesidade I")
+        break;
+
+    case imc >= 35 && imc <= 39.99:
+        console.log("Obesidade II")
+        break;
+
+    case imc >= 40 && imc <= 44.99:
+        console.log("Obesidade III")
+        break;
+
+}
+
+// console.log(typeof imc);
