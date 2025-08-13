0b237b613811f3178ec6c5a01abf3aa47ed16b59
Gabriel Correia Reis <correiareisg@gmail.com>
2025-08-12 23:12:50 -0300

    tabuada

---
+++ 006 - exercicios/002 - tabuada/index.js
@@ -0,0 +1,18 @@
+/*
+Precisamos mostrar a tabuada no console de 0 até 10 neste seguinte formato:
+
+multiplicador x número = resultado
+multiplicador x 0 = resultado
+multiplicador x 1 = resultado
+multiplicador x 2 = resultado
+multiplicador x 3 = resultado
+...
+multiplicador x 10 = resultado
+
+Obs.: não podemos criar varios consoles.log(multiplicador x 3 = resultado)
+*/
+
+const multiplicador = 7;
+
+let i = 0
+for(let i = 0)
