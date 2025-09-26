6bf271e228361bba22de20449f9983a4ff0ea4e7
Gabriel Correia Reis <correiareisg@gmail.com>
2025-09-26 17:04:33 -0300

    length com Palmeiras!

---
+++ 006 - exercicios/003 - letras repetidas/Length/index.js
@@ -0,0 +1,7 @@
+let palmeiras = [1999, 2020, 2021]
+
+console.log("Palmeiras foi campeão da Libertadores quantas vezes? " + palmeiras.length + " vezes")
+
+// ou
+
+console.log(`Palmeiras foi campeão da Libertadores quantas vezes? ${palmeiras.length} vezes`)
