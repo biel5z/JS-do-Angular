bc9cedfbd0c9e01bfaafbda8e3306b20b32a8bee
Gabriel Correia Reis <correiareisg@gmail.com>
2025-09-29 11:58:10 -0300

    letras repetidas

--- 006 - exercicios/003 - letras repetidas/index.js
+++ 006 - exercicios/003 - letras repetidas/index.js
@@ -3,4 +3,4 @@ Precisamos mostrar quantas letras repetidas aparecem em uma palavra
 exp.: Abacate = {a: 3, b: 1, c: 1, T: 1, e: 1}
 */
 const palavra = "Abacate"
-console.log(palavra)
+console.log(palavra.length)
