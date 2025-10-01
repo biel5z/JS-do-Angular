005cd1d7e95a26857f426bc9df28d26e62ded368
Gabriel Correia Reis <correiareisg@gmail.com>
2025-09-30 22:35:22 -0300

    for.js

---
+++ for.js/1-for.js
@@ -0,0 +1,23 @@
+/*
+Estrutura de Repetição - LOOP
+ -FOR
+  1) INICIALIZAÇÂO - Criar uma variável e 
+                     colocar um valor inicial pra ela.
+
+  2) CODIÇÂO - Enquanto for true, o laço continuará iterando.
+               Ele irá verificar antes de cada iteração .
+
+  3) EXPRESSÃO FINAL - O que irá ocorrer a cada vez que o 
+                       nosso laço der uma volta.
+
+
+  for ([inicialização]: [condição]: [expressão final]){
+     Seu codigo aqui
+  }                      
+*/
+
+const users = ['Maria', 'Ana', 'Gabrielle']
+
+for(let i = 0; i < users.length; i++ ){
+    console.log(users[i])
+}
