fc595ac2d7ee013173eea763b2f81fedf9b7efaa
Gabriel Correia Reis <correiareisg@gmail.com>
2025-11-25 16:11:45 -0300

    for of

---
+++ for.js/2.for of.js
@@ -0,0 +1,11 @@
+/*
+    Estruturu de Repetição - LOOP
+    - FOR OF
+    - Itera por todos os itens do array
+*/
+
+const users = ['Maria', 'Ana', 'Gabrielle']
+
+for(let name of users){
+    console.log(name)
+}
